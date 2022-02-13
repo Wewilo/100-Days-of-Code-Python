1e34a67b05b472cb55cf48551034dc4590e46b4c
Abdulkadir <74473613+Wewilo@users.noreply.github.com>
2022-02-13 12:33:06 +0000

    Added try except blocks

--- Day 26 NATO-alphabet/main.py
+++ Day 26 NATO-alphabet/main.py
@@ -1,16 +1,24 @@
 
+
 import pandas
 
 nato_alpha = pandas.read_csv("nato_phonetic_alphabet.csv") 
 
-word = input("Enter a word : ").upper()
-
 nato_alphabet = {value['letter']:value['code'] for key,value in nato_alpha.iterrows()}
 
+while True:
+    
+    word = input("Enter a word : ").upper()
 
-result = [nato_alphabet[letter] for letter in word]
+    try:
+        result = [nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print('Only letters from alphabet')
+    else:
+        print(result)
+        break
 
-print(result)
+
 
 
 
